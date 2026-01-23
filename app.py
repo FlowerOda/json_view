32c38120078468092339885b97686dbeec0bba6c
HXTFire <42458560+FlowerOda@users.noreply.github.com>
2026-01-23 09:14:50 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="front", static_url_path="")
+
+
+@app.route("/")
+def index():
+    return send_from_directory("front", "index.html")
+
+
+@app.route("/<path:path>")
+def static_proxy(path):
+    return send_from_directory("front", path)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
